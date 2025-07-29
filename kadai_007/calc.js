15e7f6b23414b0d4b08c4b431948b30e848a68bb
maotwins <suzukimao87@gmail.com>
2025-07-30 01:21:51 +0900

    全角→半角の修正

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -3,11 +3,11 @@ let num = Math.floor(Math.random() * 30)+1;
 console.log(num);
 
 if (num % 3 ==0 && num % 5 ==0) {
-  console.log('３と５の倍数です');
+  console.log('3と5の倍数です');
 } else if (num % 3 ==0) {
-  console.log('３の倍数です');
+  console.log('3の倍数です');
 } else if (num % 5 ==0) {
-  console.log('５の倍数です');
+  console.log('5の倍数です');
 } else {
   console.log(num);
 }
