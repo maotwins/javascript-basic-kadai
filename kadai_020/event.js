123539e4f2bd69871bbfbcb610bb02326cc7ae1a
maotwins <suzukimao87@gmail.com>
2025-08-15 02:16:25 +0900

    Update event.js

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,8 +1,8 @@
 // btnというidをもつHTML要素を取得し、定数に代入する
 const btn = document.getElementById('btn');
 
-  // textというidをもつHTML要素を取得し、変数に代入する
-  const text = document.getElementById('text');
+// textというidをもつHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
 
 
 // HTML要素がクリックされたときにイベント処理を実行する
